adef012b12bd7ae34d05ef4a4e39a83e86eb5685
Татьяна Михайлова <dark.little.finch@gmail.com>
2023-06-28 22:47:11 +0700

    feat: help info was added

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,22 @@
 package hexlet.code;
 
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+
+@Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
+        description = "Compares two configuration files and shows a difference.")
+
 public class App {
+
+    @Option(names = {"-h", "--help"}, usageHelp = true, description = "Shows this help message and exit.")
+    boolean usageHelpRequested;
+
+    @Option(names = {"-V", "--version"}, versionHelp = true, description = "Print version information and exit.")
+    boolean versionInfoRequested;
+
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
     }
 }
