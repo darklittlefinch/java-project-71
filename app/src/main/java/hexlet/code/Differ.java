b2d045ea43745c95c0d51817ffdfb81b3ddd10a6
Татьяна Михайлова <dark.little.finch@gmail.com>
2023-07-03 22:56:49 +0700

    fix: unchecked assignment error was fixed

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -7,10 +7,11 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import java.util.List;
 import java.util.Map;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
+import java.util.SortedSet;
+import java.util.TreeSet;
 import java.util.StringJoiner;
 
 public class Differ {
@@ -30,45 +31,60 @@ public class Differ {
         return Files.readString(filePath);
     }
 
+    // returns parameterized map from non-parameterized
+    public static Map<String, String> toParameterizedMap(Map nonParameterizedMap) {
+
+        Map<String, String> result = new HashMap<>();
+
+        for (Object key: nonParameterizedMap.keySet()) {
+            var stringKey = key.toString();
+            var stringValue = nonParameterizedMap.get(key).toString();
+            result.put(stringKey, stringValue);
+        }
+
+        return result;
+    }
+
     // Returns Map from specified JSON file
-    public static Map<String, Object> getJsonMap(String fileName) throws IOException {
+    public static Map<String, String> getJsonMap(String fileName) throws IOException {
+
         String file = readFile(fileName);
 
         ObjectMapper mapper = new ObjectMapper();
-        return mapper.readValue(file, Map.class);
+        Map result =  mapper.readValue(file, Map.class);
+
+        return toParameterizedMap(result);
     }
 
     // Returns sorted List of keys from two Maps
-    public static List<String> getSortedKeysFromMaps(Map<String, Object> map1, Map<String, Object> map2) {
-        Map<String, Object> allKeys = new HashMap<>();
-        allKeys.putAll(map1);
-        allKeys.putAll(map2);
+    public static SortedSet<String> getSortedKeysFromMaps(Map<String, String> map1, Map<String, String> map2) {
 
-        return allKeys.keySet().stream()
-                .sorted()
-                .toList();
+        SortedSet<String> sortedKeys = new TreeSet<>();
+        sortedKeys.addAll(map1.keySet());
+        sortedKeys.addAll(map2.keySet());
+
+        return sortedKeys;
     }
 
     // Returns String that contains differences of two maps
-    public static Map<String, Object> getDiffsMap(Map<String, Object> map1, Map<String, Object> map2) {
+    public static Map<String, String> getDiffsMap(Map<String, String> map1, Map<String, String> map2) {
 
-        List<String> allKeys = getSortedKeysFromMaps(map1, map2);
-
-        Map<String, Object> diffsMap = new LinkedHashMap<>();
+        SortedSet<String> allKeys = getSortedKeysFromMaps(map1, map2);
+        Map<String, String> diffsMap = new LinkedHashMap<>();
 
         for (String key: allKeys) {
             var firstMapHasKey = map1.containsKey(key);
             var secondMapHasKey = map2.containsKey(key);
             var bothMapsHaveKey = firstMapHasKey && secondMapHasKey;
 
-            Object value1 = map1.get(key);
-            Object value2 = map2.get(key);
+            String value1 = map1.get(key);
+            String value2 = map2.get(key);
 
             if (bothMapsHaveKey && value1.equals(value2)) {
                 var newKey = NOT_CHANGED_SYMBOL + " " + key;
                 diffsMap.put(newKey, value1);
 
-            } else if (bothMapsHaveKey && !value1.equals(value2)) {
+            } else if (bothMapsHaveKey) {
                 var newKey1 = DELETED_SYMBOL + " " + key;
                 var newKey2 = ADDED_SYMBOL + " " + key;
                 diffsMap.put(newKey1, value1);
@@ -86,10 +102,11 @@ public class Differ {
     }
 
     // Returns String of differences
-    public static String diffsToString(Map<String, Object> diffsMap) {
+    public static String diffsToString(Map<String, String> diffsMap) {
+
         StringJoiner sj = new StringJoiner("\n  ", "{\n  ", "\n}");
 
-        for (Map.Entry<String, Object> entry: diffsMap.entrySet()) {
+        for (Map.Entry<String, String> entry: diffsMap.entrySet()) {
             sj.add(entry.getKey() + ": " + entry.getValue());
         }
 
@@ -99,10 +116,10 @@ public class Differ {
     // Returns differences of two JSON files
     public static String generate(String fileName1, String fileName2) throws IOException {
 
-        Map<String, Object> fileContent1 = getJsonMap(fileName1);
-        Map<String, Object> fileContent2 = getJsonMap(fileName2);
+        Map<String, String> fileContent1 = getJsonMap(fileName1);
+        Map<String, String> fileContent2 = getJsonMap(fileName2);
 
-        Map<String, Object> diffsMap = getDiffsMap(fileContent1, fileContent2);
+        Map<String, String> diffsMap = getDiffsMap(fileContent1, fileContent2);
         return diffsToString(diffsMap);
     }
 }
