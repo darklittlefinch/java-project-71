5133d596e87330ea5536cf3df703ae1a14155580
Татьяна Михайлова <dark.little.finch@gmail.com>
2023-07-04 03:44:23 +0700

    fix: resources directory was changed

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -7,12 +7,14 @@ import static org.assertj.core.api.Assertions.catchThrowable;
 
 public class DifferTest {
 
-    private static final String FILEPATH_1 = "src/main/resources/files/file1.json";
-    private static final String FILEPATH_2 = "src/main/resources/files/file2.json";
+    private static final String FILEPATH_1 = "src/test/resources/files/file1.json";
+    private static final String FILEPATH_2 = "src/test/resources/files/file2.json";
+    private static final String FILEPATH_3 = "src/test/resources/files/file1.yml";
+    private static final String FILEPATH_4 = "src/test/resources/files/file2.yml";
     private static final String WRONG_PATH = "123";
 
     @Test
-    public void testGenerate() throws Exception {
+    public void testGenerateJson() throws IOException {
         String expected = """
                 {
                   - follow: false
@@ -22,12 +24,29 @@ public class DifferTest {
                   + timeout: 20
                   + verbose: true
                 }""";
-        String actual = Differ.generate(FILEPATH_1, FILEPATH_2);
-        assertThat(actual).isEqualTo(expected);
+
+        String actual1 = Differ.generate(FILEPATH_1, FILEPATH_2);
+        assertThat(actual1).isEqualTo(expected);
     }
 
     @Test
-    public void testGenerateWithWrongPath() {
+    public void testGenerateYml() throws IOException {
+        String expected = """
+                {
+                  - follow: false
+                    host: hexlet.io
+                  - proxy: 123.234.53.22
+                  - timeout: 50
+                  + timeout: 20
+                  + verbose: true
+                }""";
+
+        String actual2 = Differ.generate(FILEPATH_3, FILEPATH_4);
+        assertThat(actual2).isEqualTo(expected);
+    }
+
+    @Test
+    public void testGenerateWrongPath() {
         var thrown = catchThrowable(
                 () -> Differ.generate(WRONG_PATH, WRONG_PATH)
         );
