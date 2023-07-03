e1e799b488121ed53cadadea8411971f109fb189
Татьяна Михайлова <dark.little.finch@gmail.com>
2023-07-03 18:26:41 +0700

    fix: checkstyle errors in test were fixed

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -1,10 +1,9 @@
 package hexlet.code;
 
 import org.junit.jupiter.api.Test;
-
 import java.io.IOException;
-
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
 
 public class DifferTest {
 
