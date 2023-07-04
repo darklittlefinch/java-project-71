ac8c44a85a3f90c74862ee9a807c056fd38e26fe
Татьяна Михайлова <dark.little.finch@gmail.com>
2023-07-05 04:03:05 +0700

    fix: tests were rewritten

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -13,19 +13,38 @@ public class DifferTest {
     private static final String FILEPATH_4 = "src/test/resources/files/file2.yml";
     private static final String WRONG_PATH = "123";
 
+    private static final String FORMAT_STYLISH = "stylish";
+
     @Test
     public void testGenerateJson() throws IOException {
         String expected = """
                 {
-                  - follow: false
-                    host: hexlet.io
-                  - proxy: 123.234.53.22
-                  - timeout: 50
-                  + timeout: 20
-                  + verbose: true
+                    chars1: [a, b, c]
+                  - chars2: [d, e, f]
+                  + chars2: false
+                  - checked: false
+                  + checked: true
+                  - default: null
+                  + default: [value1, value2]
+                  - id: 45
+                  + id: null
+                  - key1: value1
+                  + key2: value2
+                    numbers1: [1, 2, 3, 4]
+                  - numbers2: [2, 3, 4, 5]
+                  + numbers2: [22, 33, 44, 55]
+                  - numbers3: [3, 4, 5]
+                  + numbers4: [4, 5, 6]
+                  + obj1: {nestedKey=value, isNested=true}
+                  - setting1: Some value
+                  + setting1: Another value
+                  - setting2: 200
+                  + setting2: 300
+                  - setting3: true
+                  + setting3: none
                 }""";
 
-        String actual1 = Differ.generate(FILEPATH_1, FILEPATH_2);
+        String actual1 = Differ.generate(FILEPATH_1, FILEPATH_2, FORMAT_STYLISH);
         assertThat(actual1).isEqualTo(expected);
     }
 
@@ -33,22 +52,39 @@ public class DifferTest {
     public void testGenerateYml() throws IOException {
         String expected = """
                 {
-                  - follow: false
-                    host: hexlet.io
-                  - proxy: 123.234.53.22
-                  - timeout: 50
-                  + timeout: 20
-                  + verbose: true
+                    chars1: [a, b, c]
+                  - chars2: [d, e, f]
+                  + chars2: false
+                  - checked: false
+                  + checked: true
+                  - default: null
+                  + default: [value1, value2]
+                  - id: 45
+                  + id: null
+                  - key1: value1
+                  + key2: value2
+                    numbers1: [1, 2, 3, 4]
+                  - numbers2: [2, 3, 4, 5]
+                  + numbers2: [22, 33, 44, 55]
+                  - numbers3: [3, 4, 5]
+                  + numbers4: [4, 5, 6]
+                  + obj1: {nestedKey=value, isNested=true}
+                  - setting1: Some value
+                  + setting1: Another value
+                  - setting2: 200
+                  + setting2: 300
+                  - setting3: true
+                  + setting3: none
                 }""";
 
-        String actual2 = Differ.generate(FILEPATH_3, FILEPATH_4);
+        String actual2 = Differ.generate(FILEPATH_3, FILEPATH_4, FORMAT_STYLISH);
         assertThat(actual2).isEqualTo(expected);
     }
 
     @Test
     public void testGenerateWrongPath() {
         var thrown = catchThrowable(
-                () -> Differ.generate(WRONG_PATH, WRONG_PATH)
+                () -> Differ.generate(WRONG_PATH, WRONG_PATH, FORMAT_STYLISH)
         );
 
         assertThat(thrown).isInstanceOf(IOException.class);
