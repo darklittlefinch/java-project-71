a323d7014632f2423edee3415a3e410e3a790cbb
Татьяна Михайлова <dark.little.finch@gmail.com>
2023-07-05 08:14:22 +0700

    add new tests with json format

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -15,6 +15,7 @@ public class DifferTest {
 
     private static final String FORMAT_STYLISH = "stylish";
     private static final String FORMAT_PLAIN = "plain";
+    private static final String FORMAT_JSON = "json";
 
     @Test
     public void testGenerateJsonStylish() throws IOException {
@@ -121,6 +122,35 @@ public class DifferTest {
                 Property 'setting3' was updated. From true to 'none'""";
 
         String actual = Differ.generate(FILEPATH_3, FILEPATH_4, FORMAT_PLAIN);
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    public void testGenerateJsonJson() throws IOException {
+
+        String expected = "{\"chars1\":\"[a, b, c]\",\"- chars2\":\"[d, e, f]\",\"+ chars2\":\"false\",\"- checked\""
+                + ":\"false\",\"+ checked\":\"true\",\"- default\":\"null\",\"+ default\":\"[value1, value2]\",\"- id\""
+                + ":\"45\",\"+ id\":\"null\",\"- key1\":\"value1\",\"+ key2\":\"value2\",\"numbers1\":\"[1, 2, 3, 4]\","
+                + "\"- numbers2\":\"[2, 3, 4, 5]\",\"+ numbers2\":\"[22, 33, 44, 55]\",\"- numbers3\":\"[3, 4, 5]\","
+                + "\"+ numbers4\":\"[4, 5, 6]\",\"+ obj1\":\"{nestedKey=value, isNested=true}\",\"- setting1\":\""
+                + "Some value\",\"+ setting1\":\"Another value\",\"- setting2\":\"200\",\"+ setting2\":\"300\","
+                + "\"- setting3\":\"true\",\"+ setting3\":\"none\"}";
+
+        String actual = Differ.generate(FILEPATH_1, FILEPATH_2, FORMAT_JSON);
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    public void testGenerateYmlJson() throws IOException {
+        String expected = "{\"chars1\":\"[a, b, c]\",\"- chars2\":\"[d, e, f]\",\"+ chars2\":\"false\",\"- checked\""
+                + ":\"false\",\"+ checked\":\"true\",\"- default\":\"null\",\"+ default\":\"[value1, value2]\",\"- id\""
+                + ":\"45\",\"+ id\":\"null\",\"- key1\":\"value1\",\"+ key2\":\"value2\",\"numbers1\":\"[1, 2, 3, 4]\","
+                + "\"- numbers2\":\"[2, 3, 4, 5]\",\"+ numbers2\":\"[22, 33, 44, 55]\",\"- numbers3\":\"[3, 4, 5]\","
+                + "\"+ numbers4\":\"[4, 5, 6]\",\"+ obj1\":\"{nestedKey=value, isNested=true}\",\"- setting1\":\""
+                + "Some value\",\"+ setting1\":\"Another value\",\"- setting2\":\"200\",\"+ setting2\":\"300\","
+                + "\"- setting3\":\"true\",\"+ setting3\":\"none\"}";
+
+        String actual = Differ.generate(FILEPATH_3, FILEPATH_4, FORMAT_JSON);
         assertThat(actual).isEqualTo(expected);
     }
 
