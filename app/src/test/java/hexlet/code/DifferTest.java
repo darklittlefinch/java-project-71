b7ae0fd87794293b039e91b926eaf64d62aefd98
Татьяна Михайлова <dark.little.finch@gmail.com>
2023-07-08 22:17:36 +0700

    fix: tests were optimezed

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -1,155 +1,111 @@
 package hexlet.code;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.StringJoiner;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.catchThrowable;
 
 public class DifferTest {
 
-    private static final String FILEPATH_1 = "src/test/resources/files/file1.json";
-    private static final String FILEPATH_2 = "src/test/resources/files/file2.json";
-    private static final String FILEPATH_3 = "src/test/resources/files/file1.yml";
-    private static final String FILEPATH_4 = "src/test/resources/files/file2.yml";
+    private static final String ROOT = "src/test/resources/files/";
+    private static final String RESULTS = "results/";
     private static final String WRONG_PATH = "123";
+
+    private static String fileJson1 = "file1.json";
+    private static String fileJson2 = "file2.json";
+    private static String fileYaml1 = "file1.yml";
+    private static String fileYaml2 = "file2.yml";
+
+    private static String stylish = "stylish";
+    private static String plain = "plain";
+    private static String json = "json";
+
+    public static String getPath(String... filesAndDirs) {
+
+        StringBuilder filepath = new StringBuilder(ROOT);
+        for (String fileOrDir: filesAndDirs) {
+            filepath.append(fileOrDir);
+        }
+        return filepath.toString();
+    }
+
+    public static String getContent(String filepath) throws IOException {
+
+        BufferedReader reader = new BufferedReader(new FileReader(filepath));
+        String lineOfFile = reader.readLine();
+
+        var result = new StringJoiner("\n");
+
+        while (lineOfFile != null) {
+            result.add(lineOfFile);
+            lineOfFile = reader.readLine();
+        }
+
+        return result.toString();
+    }
+
+    @BeforeAll
+    public static void beforeAll() {
+
+        fileJson1 = getPath(fileJson1);
+        fileJson2 = getPath(fileJson2);
+        fileYaml1 = getPath(fileYaml1);
+        fileYaml2 = getPath(fileYaml2);
+
+        stylish = getPath(RESULTS, stylish);
+        plain = getPath(RESULTS, plain);
+        json = getPath(RESULTS, json);
+    }
 
     @Test
     public void testGenerateJson() throws IOException {
-        String expected = """
-                {
-                    chars1: [a, b, c]
-                  - chars2: [d, e, f]
-                  + chars2: false
-                  - checked: false
-                  + checked: true
-                  - default: null
-                  + default: [value1, value2]
-                  - id: 45
-                  + id: null
-                  - key1: value1
-                  + key2: value2
-                    numbers1: [1, 2, 3, 4]
-                  - numbers2: [2, 3, 4, 5]
-                  + numbers2: [22, 33, 44, 55]
-                  - numbers3: [3, 4, 5]
-                  + numbers4: [4, 5, 6]
-                  + obj1: {nestedKey=value, isNested=true}
-                  - setting1: Some value
-                  + setting1: Another value
-                  - setting2: 200
-                  + setting2: 300
-                  - setting3: true
-                  + setting3: none
-                }""";
 
-        String actual = Differ.generate(FILEPATH_1, FILEPATH_2, Formatter.FORMAT_STYLISH);
+        String expected = getContent(stylish);
+        String actual = Differ.generate(fileJson1, fileJson2, Formatter.FORMAT_STYLISH);
         assertThat(actual).isEqualTo(expected);
     }
 
     @Test
     public void testGenerateYml() throws IOException {
-        String expected = """
-                {
-                    chars1: [a, b, c]
-                  - chars2: [d, e, f]
-                  + chars2: false
-                  - checked: false
-                  + checked: true
-                  - default: null
-                  + default: [value1, value2]
-                  - id: 45
-                  + id: null
-                  - key1: value1
-                  + key2: value2
-                    numbers1: [1, 2, 3, 4]
-                  - numbers2: [2, 3, 4, 5]
-                  + numbers2: [22, 33, 44, 55]
-                  - numbers3: [3, 4, 5]
-                  + numbers4: [4, 5, 6]
-                  + obj1: {nestedKey=value, isNested=true}
-                  - setting1: Some value
-                  + setting1: Another value
-                  - setting2: 200
-                  + setting2: 300
-                  - setting3: true
-                  + setting3: none
-                }""";
 
-        String actual = Differ.generate(FILEPATH_3, FILEPATH_4, Formatter.FORMAT_STYLISH);
+        String expected = getContent(stylish);
+        String actual = Differ.generate(fileYaml1, fileYaml2, Formatter.FORMAT_STYLISH);
         assertThat(actual).isEqualTo(expected);
     }
 
     @Test
     public void testGenerateDefaultFormat() throws IOException {
-        String expected = """
-                {
-                    chars1: [a, b, c]
-                  - chars2: [d, e, f]
-                  + chars2: false
-                  - checked: false
-                  + checked: true
-                  - default: null
-                  + default: [value1, value2]
-                  - id: 45
-                  + id: null
-                  - key1: value1
-                  + key2: value2
-                    numbers1: [1, 2, 3, 4]
-                  - numbers2: [2, 3, 4, 5]
-                  + numbers2: [22, 33, 44, 55]
-                  - numbers3: [3, 4, 5]
-                  + numbers4: [4, 5, 6]
-                  + obj1: {nestedKey=value, isNested=true}
-                  - setting1: Some value
-                  + setting1: Another value
-                  - setting2: 200
-                  + setting2: 300
-                  - setting3: true
-                  + setting3: none
-                }""";
 
-        String actual = Differ.generate(FILEPATH_1, FILEPATH_2);
+        String expected = getContent(stylish);
+        String actual = Differ.generate(fileJson1, fileJson2);
         assertThat(actual).isEqualTo(expected);
     }
 
     @Test
     public void testGeneratePlainFormat() throws IOException {
-        String expected = """
-                Property 'chars2' was updated. From [complex value] to false
-                Property 'checked' was updated. From false to true
-                Property 'default' was updated. From null to [complex value]
-                Property 'id' was updated. From 45 to null
-                Property 'key1' was removed
-                Property 'key2' was added with value: 'value2'
-                Property 'numbers2' was updated. From [complex value] to [complex value]
-                Property 'numbers3' was removed
-                Property 'numbers4' was added with value: [complex value]
-                Property 'obj1' was added with value: [complex value]
-                Property 'setting1' was updated. From 'Some value' to 'Another value'
-                Property 'setting2' was updated. From 200 to 300
-                Property 'setting3' was updated. From true to 'none'""";
 
-        String actual = Differ.generate(FILEPATH_1, FILEPATH_2, Formatter.FORMAT_PLAIN);
+        String expected = getContent(plain);
+        String actual = Differ.generate(fileJson1, fileJson2, Formatter.FORMAT_PLAIN);
         assertThat(actual).isEqualTo(expected);
     }
 
     @Test
     public void testGenerateJsonFormat() throws IOException {
 
-        String expected = "{\"chars1\":\"[a, b, c]\",\"- chars2\":\"[d, e, f]\",\"+ chars2\":\"false\",\"- checked\""
-                + ":\"false\",\"+ checked\":\"true\",\"- default\":\"null\",\"+ default\":\"[value1, value2]\",\"- id\""
-                + ":\"45\",\"+ id\":\"null\",\"- key1\":\"value1\",\"+ key2\":\"value2\",\"numbers1\":\"[1, 2, 3, 4]\","
-                + "\"- numbers2\":\"[2, 3, 4, 5]\",\"+ numbers2\":\"[22, 33, 44, 55]\",\"- numbers3\":\"[3, 4, 5]\","
-                + "\"+ numbers4\":\"[4, 5, 6]\",\"+ obj1\":\"{nestedKey=value, isNested=true}\",\"- setting1\":\""
-                + "Some value\",\"+ setting1\":\"Another value\",\"- setting2\":\"200\",\"+ setting2\":\"300\","
-                + "\"- setting3\":\"true\",\"+ setting3\":\"none\"}";
-
-        String actual = Differ.generate(FILEPATH_1, FILEPATH_2, Formatter.FORMAT_JSON);
+        String expected = getContent(json);
+        String actual = Differ.generate(fileJson1, fileJson2, Formatter.FORMAT_JSON);
         assertThat(actual).isEqualTo(expected);
     }
 
     @Test
     public void testGenerateWrongPath() {
+
         var thrown = catchThrowable(
                 () -> Differ.generate(WRONG_PATH, WRONG_PATH, Formatter.FORMAT_STYLISH)
         );
