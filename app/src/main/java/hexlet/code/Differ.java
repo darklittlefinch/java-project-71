e84e585983293a2d8dfe1e0e2c91d63c55c90bf2
Татьяна Михайлова <dark.little.finch@gmail.com>
2023-07-13 16:07:21 +0700

    style: class Parser now has only parsering methods

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -2,6 +2,10 @@ package hexlet.code;
 
 import java.io.IOException;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import java.util.List;
 import java.util.Map;
 import java.util.HashMap;
@@ -14,14 +18,18 @@ public class Differ {
     public static final String STATUS_DELETED = "deleted";
     public static final String STATUS_ADDED = "added";
 
+
     // Returns differences of two JSON files
     public static String generate(String fileName1, String fileName2, String format) throws IOException {
 
-        Map<String, Object> fileContent1 = Parser.parse(fileName1);
-        Map<String, Object> fileContent2 = Parser.parse(fileName2);
+        String content1 = readFile(fileName1);
+        String content2 = readFile(fileName2);
 
-        Map<String, String> diffMap = genDiff(fileContent1, fileContent2);
-        return Formatter.format(fileContent1, fileContent2, diffMap, format);
+        Map<String, Object> mapContent1 = Parser.parse(content1, getFileExtension(fileName1));
+        Map<String, Object> mapContent2 = Parser.parse(content2, getFileExtension(fileName2));
+
+        Map<String, String> diffMap = genDiff(mapContent1, mapContent2);
+        return Formatter.format(mapContent1, mapContent2, diffMap, format);
     }
 
     public static String generate(String fileName1, String fileName2) throws IOException {
@@ -72,5 +80,26 @@ public class Differ {
         return allKeys.keySet().stream()
                 .sorted()
                 .toList();
+    }
+
+    public static String getFileExtension(String fileName) throws IOException {
+        if (fileName.endsWith(Parser.JSON)) {
+            return Parser.JSON;
+        } else if (fileName.endsWith(Parser.YAML) || fileName.endsWith(Parser.YML)) {
+            return Parser.YAML;
+        } else {
+            throw new IOException("Unsupported extension");
+        }
+    }
+
+    // Returns content of specified file
+    public static String readFile(String fileName) throws IOException {
+        Path filePath = getPath(fileName);
+        return Files.readString(filePath);
+    }
+
+    // Returns path of specified file
+    public static Path getPath(String fileName) {
+        return Paths.get(fileName).toAbsolutePath().normalize();
     }
 }
--- app/src/main/java/hexlet/code/Parser.java
+++ app/src/main/java/hexlet/code/Parser.java
@@ -4,54 +4,38 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 import java.util.Map;
 
 public class Parser {
 
     // Supported extensions
-    private static final String JSON = ".json";
-    private static final String YAML = ".yaml";
-    private static final String YML = ".yml";
+    public static final String JSON = "json";
+    public static final String YAML = "yaml";
+    public static final String YML = "yml";
 
     // Return parsed file
-    public static Map<String, Object> parse(String fileName) throws IOException {
-        if (fileName.endsWith(JSON)) {
-            return parseJson(fileName);
-        } else if (fileName.endsWith(YML) || fileName.endsWith(YAML)) {
-            return parseYaml(fileName);
+    public static Map<String, Object> parse(String content, String extension) throws IOException {
+        if (extension.equals(JSON)) {
+            return parseJson(content);
+        } else if (extension.endsWith(YML) || extension.endsWith(YAML)) {
+            return parseYaml(content);
         } else {
             throw new IOException("Unsupported extension");
         }
     }
 
     // Returns Map from specified JSON file
-    public static Map<String, Object> parseJson(String fileName) throws IOException {
-        String file = readFile(fileName);
+    public static Map<String, Object> parseJson(String content) throws IOException {
 
         ObjectMapper mapper = new ObjectMapper();
-        return mapper.readValue(file, Map.class);
+        return mapper.readValue(content, Map.class);
     }
 
     // Returns Map from specified YAML file
-    public static Map<String, Object> parseYaml(String fileName) throws IOException {
-        String file = readFile(fileName);
+    public static Map<String, Object> parseYaml(String content) throws IOException {
 
         ObjectMapper mapper = new YAMLMapper();
-        return mapper.readValue(file, Map.class);
-    }
-
-    // Returns content of specified file
-    public static String readFile(String fileName) throws IOException {
-        Path filePath = getPath(fileName);
-        return Files.readString(filePath);
-    }
-
-    // Returns path of specified file
-    public static Path getPath(String fileName) {
-        return Paths.get(fileName).toAbsolutePath().normalize();
+        return mapper.readValue(content, Map.class);
     }
 }
